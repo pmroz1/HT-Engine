7df35ed822fa0e674728510dd9d370e9cad705d0
unknown <p.mroz20@outlook.com>
2020-10-22 14:28:21 +0200

    added basic window creation

--- Class1.cs
+++ Class1.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace HT_Engine
 {
-    public class Class1
+    public struct Game
     {
+        public GameWindow game;
+
+        public Game(string gameTitle)
+        {
+            game = new GameWindow();
+            game.Title = gameTitle;
+        }
+
+        public void Run()
+        {
+            //makes game run at 60 fps
+            game.Run(60.0);
+        }
     }
 }
