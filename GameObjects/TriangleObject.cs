888b1ad849761c25cae58df0fd9c654caed0154f
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2020-10-24 15:36:24 +0200

    added traingle GameObject

---
+++ GameObjects/TriangleObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using OpenTK.Graphics.OpenGL;
+
+namespace HT_Engine.GameObjects
+{
+    public struct TriangleObject : IGameObject
+    {
+        public Color color;
+        public TriangleObject(Color clr)
+        {
+            color = clr;
+        }
+
+        void IGameObject.RenderObject()
+        {
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Vertex2(-1.0f,-1.0f);
+            GL.Vertex2(0.0f,1.0f);
+            GL.Vertex2(1.0f,-1.0f);
+            GL.End();
+        }
+    }
+}
+
