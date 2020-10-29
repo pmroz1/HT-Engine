cf9e68f066f0926e5b591babb28cc2eb6439fdda
unknown <p.mroz20@outlook.com>
2020-10-30 00:21:21 +0100

    added actor, animation, animationset classes and interfaces

---
+++ GameObjects/Actors/Actor2D.cs
@@ -0,0 +1,39 @@
+﻿using HT_Engine.Animations;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HT_Engine.GameObjects.Actors
+{
+    public class Actor2D : IActor
+    {
+        public string actorName;
+        public Vector2 actorPosition;
+        public AnimationSet animations;
+
+        public Actor2D(Vector2 position, string name)
+        {
+            actorPosition = position;
+            actorName = name;
+        }
+
+        public Actor2D(Vector2 position, string name, AnimationSet animation)
+        {
+            actorPosition = position;
+            actorName = name;
+            this.animations = animation;
+        }
+
+        public void RenderObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateObject()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameObjects/Actors/IActor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HT_Engine.GameObjects.Actors
+{
+    public interface IActor
+    {
+        void RenderObject();
+        void UpdateObject();
+    }
+}
